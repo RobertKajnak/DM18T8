b9cd5425e2464188e5f8438442648fb7971c19ea
robertkajnak <hezirisz@gmail.com>
2018-04-21 14:57:41 +0200

    removed the 'main' section to keep variables in workspace

--- testscript.py
+++ testscript.py
@@ -2,21 +2,21 @@ from helperFunctions import plotPatient
 from getDataset import getDataset
 
 
-def main():
-    ''' dataset == a list of patient talbes, i.e.:
-     nr_patients x [nr_days_for_that_patient x nr_of_attributes]
-     more precisely: 23 x [days(varies) x 40]
-     nr_patients is a list
-     [nr_days_for_that_patient x nr_of_attributes] should be and n-d-python array
-     
-     attrubiteList == list of the attributes for the columns, i.e.:
-     ['mood','mood_count',...,'sleep','nightlife']'''
-    [dataset,attributeList] = getDataset()
-    
-    
-    '''First parameter is the patient to be plotted, second is the titles for the
-    graphs i.e. the attribute names'''
-    plotPatient(dataset[0],attributeList)
+#def main():
+''' dataset == a list of patient talbes, i.e.:
+ nr_patients x [nr_days_for_that_patient x nr_of_attributes]
+ more precisely: 23 x [days(varies) x 40]
+ nr_patients is a list
+ [nr_days_for_that_patient x nr_of_attributes] should be and n-d-python array
+ 
+ attrubiteList == list of the attributes for the columns, i.e.:
+ ['mood','mood_count',...,'sleep','nightlife']'''
+[dataset,attributeList] = getDataset()
 
-if __name__ == '__main__':
-  main()
+
+'''First parameter is the patient to be plotted, second is the titles for the
+graphs i.e. the attribute names'''
+plotPatient(dataset[0],attributeList)
+
+#if __name__ == '__main__':
+#  main()
